6991dd257c72756004cc152633c168dc9251d8ac
codeArt001 <olaniyanoluwajomiloju25@gmail.com>
2025-10-03 13:03:33 +0100

    initial commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,7 +8,7 @@ import logo from "../assets/images/LOGo.png";
 const Sidebar = () => {
   return (
     <div className="min-h-screen flex flex-col gap-10  cursor-pointer  px-5 py-5 shadow-2xl shadow-violet-500 border-white bg-white-50 w-[10%]">
-      <img src={logo} alt="logo" className="w-25 h-10 cursor-pointer" />
+      <img src={logo} alt="logo" className="w-27 h-10 cursor-pointer" />
       <MdAddHomeWork className="w-15 h-15 cursor-pointer" />
       <MdHomeRepairService className="w-15 h-15 cursor-pointer" />
       <MdEventAvailable className="w-15 h-15 cursor-pointer" />
